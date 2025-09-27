ef74ca6d411162b51c19892fe31f51c5a6ce5e96
xJesusx0 <snowcatyt1234@gmail.com>
2025-09-27 08:14:09 -0500

    fix(config): corregir valores hardcodeados en la configuracion

--- src/app/core/settings.py
+++ src/app/core/settings.py
@@ -1,14 +1,16 @@
+import os
+
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_host: str = "0.0.0.0"
-    app_port: int = 8000
-    env: str = "development"
+    app_host: str = os.getenv("APP_HOST")
+    app_port: int = os.getenv("APP_PORT")
+    env: str = os.getenv("ENV")
+    jwt_secret_key: str = os.getenv("JWT_SECRET_KEY")
+    jwt_algorithm: str = os.getenv("JWT_ALGORITHM")
+    jwt_expiration_time: int = int(os.getenv("JWT_EXPIRATION_TIME_MINUTES", 30))
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
-
-
+load_dotenv()
 settings = Settings()
