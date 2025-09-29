0c631d50e15d910e3af3c0559b52549cdcd25832
xJesusx0 <snowcatyt1234@gmail.com>
2025-09-28 21:46:42 -0500

    feat(cors): agregar configuracion de cors, hosts cargados apartir de una variable de entorno

--- src/app/core/settings.py
+++ src/app/core/settings.py
@@ -1,18 +1,38 @@
-import os
+# src/app/core/settings.py
 
 from dotenv import load_dotenv
-from pydantic_settings import BaseSettings
+from pydantic import field_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
-    app_host: str = os.getenv("APP_HOST", "localhost")
-    app_port: int = int(os.getenv("APP_PORT", "8080"))
-    env: str = os.getenv("ENV", "production")
-    jwt_secret_key: str = os.getenv("JWT_SECRET_KEY", "secret")
-    jwt_algorithm: str = os.getenv("JWT_ALGORITHM", "HS256")
-    jwt_expiration_time: int = int(os.getenv("JWT_EXPIRATION_TIME_DAYS", 1))
-    db_url: str = os.getenv("DB_URL", "")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
+
+    # Proporciona los valores por defecto directamente.
+    app_host: str = "localhost"
+    app_port: int = 8080
+    env: str = "production"
+    jwt_secret_key: str = "secret"
+    jwt_algorithm: str = "HS256"
+    jwt_expiration_time: int = 1  # tiempo en dias
+    db_url: str = ""
+
+    allowed_hosts: list[str] = []
+
+    @field_validator("allowed_hosts", mode="before")
+    @classmethod
+    def build_allowed_hosts(cls, value: str | list[str]) -> str | list[str]:
+        if isinstance(value, str) and value:
+            return [host.strip() for host in value.split(",")]
+
+        # Si el valor es una lista (o nulo/vacío), lo devuelve tal cual.
+        if isinstance(value, list):
+            return value
+        return []  # Devuelve lista vacía si no hay nada
 
 
-load_dotenv()
 settings = Settings()
